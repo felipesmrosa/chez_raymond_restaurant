e2961cd26f1b6877d0238625e5fbdf4419eba5a1
Felipe Miranda <felipem04.r@gmail.com>
2025-01-23 19:35:02 -0300

    ajustes css, agora parece estar tudo certo pro cliente

--- chez_raymond/src/Components/Cardapio/Index.jsx
+++ chez_raymond/src/Components/Cardapio/Index.jsx
@@ -3,15 +3,13 @@ import menu from "@/menu.pdf"
 export function Cardapio() {
 
     return (
-        <section className="menu-section">
-            <div className="container">
-                <h1>Nosso Cardápio</h1>
-                <p className="menu-subtitle">
-                    Explore as delícias do nosso cardápio.
-                    <br />
-                    Com opções para todos os gostos, estamos prontos para surpreender você com sabores incríveis!
-                </p>
-                <a target="_blank" href={menu}>Cardápio Completo</a>
+        <section className="cardapio">
+            <div className="cardapio__container">
+                <h1>conheça Nosso Cardápio</h1>
+                <h4 className="cardapio__container--subtitulo">
+                    Sabor para todos gostos
+                </h4>
+                <a className="cardapio__container--button" target="_blank" href={menu}>Cardápio Completo</a>
             </div>
         </section>
     );
